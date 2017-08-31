1852757a8a3adbaf34b63369e8fb162d026c68f8
ZijieYuan <zyuan18@asu.edu>
2017-08-31 10:27:02 -0700

    Update sec_preprocessing.py

--- slim/preprocessing/sec_preprocessing.py
+++ slim/preprocessing/sec_preprocessing.py
@@ -11,7 +11,7 @@ slim = tf.contrib.slim
 
 
 def preprocess_image(image, output_height, output_width, is_training):
-  """Preprocesses the given image.
+  """Data argumentation. Produce child images from a same input image.
 
   Args:
     image: A `Tensor` representing an image of arbitrary size.
@@ -21,15 +21,22 @@ def preprocess_image(image, output_height, output_width, is_training):
       `False` otherwise.
 
   Returns:
-    A secondary preprocessed images (an image list).
+    image_list - A secondary preprocessed images (an image list of child images).
+    num_child_image - The number of child images.
+    DA_flag - A flag that indicates this function has been called.
   """
+# Set a flag indecating the function has been called.
+  DA_flag = 1
+
 # Transform the image to floats.
   image = tf.to_float(image)
   
 # Rotation
-  angle = [np.pi/2,np.pi,1.5*np.pi]    # rotate to 3 other orientations (4 ori in total)
+  # angle = [np.pi/4,np.pi/2,0.75*np.pi,np.pi,1.25*np.pi,1.5*np.pi,1.75*np.pi]    # rotate to 7 other orientations (4 ori in total)
+  angle = [np.pi/2,np.pi,1.5*np.pi] 
   image_list = [image]    # initialize image_list; save orig image into image_list
+  num_child_image = len(angle) + 1
   for ang in angle:
-      image_single = tf.contrib.image.rotate(image,angle)
-      image_list = images.append(image_single)
-  return image_list
+      image_single = tf.contrib.image.rotate(image,ang)
+      image_list.append(image_single)
+  return(image_list, num_child_image, DA_flag)
